0aca36a0254a57827e63baff2a9323256a628281
VinamraGupta20 <160421475+VinamraGupta20@users.noreply.github.com>
2026-04-10 19:55:15 +0530

    Refactor settings loading and slider functionality
    Refactor loading settings and update slider behavior.

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ const toggle = document.getElementById("modeToggle");
 const slider = document.getElementById("slider");
 const value = document.getElementById("value");
 
-// Load settings
+
 chrome.storage.local.get(["mode", "visibleLimit"], (res) => {
     const mode = res.mode || "fast";
     const visible = res.visibleLimit || 3;
@@ -12,18 +12,18 @@ chrome.storage.local.get(["mode", "visibleLimit"], (res) => {
     slider.value = visible;
     value.innerText = visible;
 
-    // 🔥 disable slider in full mode
+
     slider.disabled = (mode === "full");
 });
 
-// Toggle
+
 toggle.onchange = () => {
     const mode = toggle.checked ? "full" : "fast";
 
     chrome.storage.local.set({ mode });
 };
 
-// Slider
+
 slider.oninput = () => {
     const val = parseInt(slider.value);
 
